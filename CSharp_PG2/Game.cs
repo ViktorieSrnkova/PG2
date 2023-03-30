fa52daad757e7fa31cc9410a3b69ca2bc79a1641
ViktorieSrnkova <81012690+ViktorieSrnkova@users.noreply.github.com>
2023-03-30 16:33:49 +0200

    first commit

---
+++ CSharp_PG2/Game.cs
@@ -0,0 +1,30 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace CSharp_PG2
+{
+    class Game : GameWindow
+    {
+        public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) { }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            KeyboardState input = KeyboardState;
+
+            if (input.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ CSharp_PG2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharp_PG2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game game = new Game(800, 600, "LearnOpenTK"))
+            {
+                game.Run();
+                
+            }
+
+        }
+    }
+}
