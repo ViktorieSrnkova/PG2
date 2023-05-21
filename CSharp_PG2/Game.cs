ca81e52bcad6bb2b88866bc1686aa99ba7523fa7
ViktorieSrnkova <81012690+ViktorieSrnkova@users.noreply.github.com>
2023-05-21 10:41:01 +0200

    Remove unnecessary files

--- CSharp_PG2/Game.cs
+++ CSharp_PG2/Game.cs
@@ -15,35 +15,50 @@ class Game : GameWindow
     private const float FOV = 90;
 
     private readonly float[] _vertices =
-    {   // 3x position         //3x color                //2x tex coords
-        -0.5f, 0.0f, 0.5f,     0.83f, 0.70f, 0.44f,      0.0f, 0.0f,
-        -0.5f, 0.0f, -0.5f,    0.83f, 0.70f, 0.44f,      5.0f, 0.0f,
-        0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,      0.0f, 0.0f,
-        0.5f, 0.0f, 0.5f,      0.83f, 0.70f, 0.44f,      5.0f, 0.0f,
-        0.0f, 0.8f, 0.0f,      0.83f, 0.70f, 0.44f,      5.0f, 0.0f
+    {   //     COORDINATES     /        COLORS          /    TexCoord   /        NORMALS       //
+        -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 	 0.0f, 0.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+        -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 0.0f, 5.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+        0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 5.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+        0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+
+        -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 	 0.0f, 0.0f,     -0.8f, 0.5f,  0.0f, // Left Side
+        -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,     -0.8f, 0.5f,  0.0f, // Left Side
+        0.0f, 0.8f,  0.0f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,     -0.8f, 0.5f,  0.0f, // Left Side
+
+        -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, 0.5f, -0.8f, // Non-facing side
+        0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 0.0f, 0.0f,      0.0f, 0.5f, -0.8f, // Non-facing side
+        0.0f, 0.8f,  0.0f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, 0.5f, -0.8f, // Non-facing side
+
+        0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 0.0f, 0.0f,      0.8f, 0.5f,  0.0f, // Right side
+        0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.8f, 0.5f,  0.0f, // Right side
+        0.0f, 0.8f,  0.0f,    0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.8f, 0.5f,  0.0f, // Right side
+
+        0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, 0.5f,  0.8f, // Facing side
+        -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 	 0.0f, 0.0f,      0.0f, 0.5f,  0.8f, // Facing side
+        0.0f, 0.8f,  0.0f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, 0.5f,  0.8f  // Facing side
     };
 
     private readonly uint[] _indices =
     {
-        0, 1, 2,
-        0, 2, 3,
-        0, 1, 4,
-        1, 2, 4,
-        2, 3, 4,
-        3, 0, 4
+        0, 1, 2, // Bottom side
+        0, 2, 3, // Bottom side
+        4, 6, 5, // Left side
+        7, 9, 8, // Non-facing side
+        10, 12, 11, // Right side
+        13, 15, 14 // Facing side
     };
 
     private readonly float[] _lightVertices =
     {
         //COORDINATES//         //color                         //poz
-        -0.1f, -0.1f, 0.1f,     1.0f, 1.0f, 1.0f,         0.9f, 0.9f,
-        -0.1f, -0.1f, -0.1f,    1.0f, 1.0f, 1.0f,         0.9f, 0.9f,
-        0.1f, -0.1f, -0.1f,     1.0f, 1.0f, 1.0f,         0.9f, 0.9f,
-        0.1f, -0.1f, 0.1f,      1.0f, 1.0f, 1.0f,         0.9f, 0.9f,
-        -0.1f, 0.1f, 0.1f,      1.0f, 1.0f, 1.0f,         0.9f, 0.9f,
-        -0.1f, 0.1f, -0.1f,     1.0f, 1.0f, 1.0f,         0.9f, 0.9f,
-        0.1f, 0.1f, -0.1f,      1.0f, 1.0f, 1.0f,         0.9f, 0.9f,
-        0.1f, 0.1f, 0.1f,        1.0f, 1.0f, 1.0f,        0.9f, 0.9f
+        -0.1f + 1, -0.1f + 1, 0.1f + 1,     1.0f, 1.0f, 1.0f,         0.9f, 0.9f,
+        -0.1f + 1, -0.1f + 1, -0.1f + 1,    1.0f, 1.0f, 1.0f,         0.9f, 0.9f,
+        0.1f + 1, -0.1f + 1, -0.1f +1,     1.0f, 1.0f, 1.0f,         0.9f, 0.9f,
+        0.1f + 1, -0.1f + 1, 0.1f + 1,      1.0f, 1.0f, 1.0f,         0.9f, 0.9f,
+        -0.1f + 1, 0.1f + 1, 0.1f + 1,      1.0f, 1.0f, 1.0f,         0.9f, 0.9f,
+        -0.1f + 1, 0.1f + 1, -0.1f + 1,     1.0f, 1.0f, 1.0f,         0.9f, 0.9f,
+        0.1f + 1, 0.1f + 1, -0.1f + 1,      1.0f, 1.0f, 1.0f,         0.9f, 0.9f,
+        0.1f + 1, 0.1f + 1, 0.1f + 1,        1.0f, 1.0f, 1.0f,        0.9f, 0.9f
 
     };
 
